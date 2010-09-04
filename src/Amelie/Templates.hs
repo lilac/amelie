d30531daa70681bc2a5e5218ec35ab8387f77cc3
Chris Done <chris.done@create-net.org>
2010-09-04 12:06:16 +0200

    Refactored template function using renderTemplate

--- src/Amelie/Templates.hs
+++ src/Amelie/Templates.hs
@@ -1,57 +1,51 @@
 {-# LANGUAGE FlexibleContexts, NamedFieldPuns #-}
 module Amelie.Templates where
 
-import           Control.Applicative      ((<$>),(<*>))
-import           Control.Monad.State      (MonadState)
-import           Control.Monad.State      (gets)
-import           Control.Monad.Trans      (MonadIO)
-import           Control.Monad.Trans      (liftIO)
-import qualified Data.ByteString.Char8    as B (ByteString)
-import qualified Data.ByteString.Char8    as B (readFile,pack)
-import qualified Data.ByteString.Lazy     as L (ByteString)
-import qualified Data.ByteString.Lazy     as L (fromChunks)
-import           Data.Char                (toUpper)
-import           Data.List                (foldl')
-import           System.Directory         (doesFileExist)
+import           Control.Applicative        ((<$>),(<*>))
+import           Control.Monad.State        (MonadState)
+import           Control.Monad.State        (gets)
+import           Control.Monad.Trans        (MonadIO)
+import           Control.Monad.Trans        (liftIO)
+import qualified Data.ByteString.Char8      as B (ByteString)
+import qualified Data.ByteString.Char8      as B (readFile,pack)
+import qualified Data.ByteString.Lazy       as L (ByteString)
+import qualified Data.ByteString.Lazy       as L (fromChunks)
+import qualified Data.ByteString.Lazy.Char8 as L (pack)
+import           Data.Char                  (toUpper)
+import           Data.List                  (foldl')
+import           System.Directory           (doesFileExist)
 
-import           Codec.Binary.UTF8.String (encodeString)
-import           Data.ByteString.Search   (replace)
-import           Network.CGI              (CGIResult)
-import qualified Network.CGI              as CGI
-import           Network.CGI.Monad        (MonadCGI(..))
-import           System.FilePath          ((</>))
-import qualified Text.Blaze.Html5         as H
-import           Text.Blaze.Renderer.Utf8 (renderHtml)
+import           Codec.Binary.UTF8.String   (encodeString)
+import           Data.ByteString.Search     (replace)
+import           Network.CGI                (CGIResult)
+import qualified Network.CGI                as CGI
+import           Network.CGI.Monad          (MonadCGI(..))
+import           System.FilePath            ((</>))
+import qualified Text.Blaze.Html5           as H
+import           Text.Blaze.Renderer.Utf8   (renderHtml)
 
-import           Amelie.Pages.Error       (errorPage)
-import           Amelie.Types             (State(..),Title,PageName,Config(..))
-import           Amelie.Utils             (l2s)
+import           Amelie.Pages.Error         (errorPage)
+import           Amelie.Types               (State(..),Title,PageName,Config(..))
+import           Amelie.Utils               (l2s)
 
 -- | Put a rendered page into its corresponding HTML template.
-template :: (MonadState State m,MonadCGI m,MonadIO m)
+template :: (Functor m,MonadState State m,MonadCGI m,MonadIO m)
             => Title -> PageName -> [(String,B.ByteString)] -> Maybe H.Html
             -> m CGIResult
 template title' name ps inner = do
-  tempDir <- gets $ templateDir . config
-  analytics <- gets $ analytics . config
-  let (temp,page) = (tempDir </> "template.html",tempDir </> name ++ ".html")
-  exists <- liftIO $ (&&) <$> doesFileExist temp 
-            <*> doesFileExist page
-  if exists
-     then do templ <- liftIO $ B.readFile page
-             mainTempl <- liftIO $ B.readFile temp
-             let params = [("page",templ)
-                          ,("name",B.pack name)
-                          ,("title",B.pack $ encodeString title')
-                          ,("analytics",B.pack analytics)]
-                          ++ ps ++ renderedHtml
-             CGI.outputFPS $ fillTemplate params mainTempl
-     else maybe (errorPage $ "No template for " ++ name) 
-                (CGI.outputFPS . renderHtml) 
-                inner
-    where renderedHtml = case inner of
-            Just in' -> [("inner",l2s $ renderHtml in')]
-            Nothing -> []
+    anal <- gets $ analytics . config
+    page <- either L.pack id <$> renderTemplate name params
+    main <- renderTemplate "template" $ params ++
+               [("page",l2s page),("analytics",B.pack anal)]
+    case main of
+      Right page' -> CGI.outputFPS page'
+      Left err    -> maybe (errorPage err) (CGI.outputFPS . renderHtml) inner
+  where params = [("name",B.pack name)
+                 ,("title",B.pack $ encodeString title')]
+                 ++ ps ++ renderedHtml
+        renderedHtml = case inner of
+          Just in' -> [("inner",l2s $ renderHtml in')]
+          Nothing -> []
 
 -- | Fill a template by a filename and render it to a string.
 renderTemplate :: (MonadIO m,MonadState State m)
